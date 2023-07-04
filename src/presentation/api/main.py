079e558753c5703e06cbed23da619a55df588198
hhssome <artempavel-terekhovy@mail.ru>
2023-07-04 19:05:55 +0300

    changes

--- src/presentation/api/main.py
+++ src/presentation/api/main.py
@@ -19,14 +19,3 @@ def build_app(config: Config) -> FastAPI:
     setup_di(app=app, config=config, pool=create_pool(config.db))
 
     return app
-
-
-async def run_api(app: FastAPI, api_config: APIConfig) -> None:
-    config_uvicorn = uvicorn.Config(
-        app=app,
-        host=api_config.host,
-        port=api_config.port,
-        reload=True,
-    )
-    server = uvicorn.Server(config_uvicorn)
-    await server.serve()
